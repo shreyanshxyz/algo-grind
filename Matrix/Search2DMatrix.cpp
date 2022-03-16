67d8ffcf0e4f441c986937ead870fe135f20f4e7
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2022-03-16 21:40:55 +0530

    Searching in a 2d matrix

--- Matrix/Search2DMatrix.cpp
+++ Matrix/Search2DMatrix.cpp
@@ -5,7 +5,7 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int r = matrix.size();
         int c = matrix[0].size();
-        
+        // a basic brute force approach where we traverse through the matrix and see if the element exists.
         for(int i = 0; i < r; i++){
             for(int j = i; j < c; j++){
                 if (matrix[i][j] == target){
@@ -26,16 +26,16 @@ public:
 
     bool search(vector<vector<int> > matrix, int n, int m, int x) 
     {
-        // code here 
-        int i = 0, j = matrix.size() - 1;
+        // this is our better approach. since the matrix is sorted row & column wise. Dry running will give a clearer idea.
+        int i = 0, j = matrix.size() - 1; // we take I as the first element of each row and j as the last one.
         while(i >= 0 && j >= 0 && j < m && i < n){
-            if(matrix[i][j] == x){
+            if(matrix[i][j] == x){ // if the current element is equal to our value we return true.
                 return 1;
             }
-            if(matrix[i][j] > x){
+            if(matrix[i][j] > x){ // if it is smaller then we move a column to the right and check again 
                 j--;
             }
-            else{
+            else{ // but in case if it is smaller we then move a row lower and then check again. 
                 i++;
             }
         }

b0fde6e8c2c8ff02ac2260ec1059699db81b0844
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-04-02 19:50:02 +0530

    Time: 20 ms (82.36%), Space: 17 MB (56.61%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -19,22 +19,23 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
+//         Right to left bfs
         if (!root) return nullptr;
         queue<Node*>q;
         q.push(root);
         
         while(!q.empty()){
             int sz = q.size();
-            Node* rn = nullptr;
+            Node* rn = nullptr; // we create a rightnode and set it to null
             for(int i = 0; i < sz; i++){
-                Node* cur = q.front();
+                Node* cur = q.front(); // create a cur node to handle every current node
                 q.pop();
-                cur -> next = rn;
-                rn = cur;
+                cur -> next = rn; // set its next to the null (rightnode as of now)
+                rn = cur; // set cur as rightnode
                 
-                if(cur -> right){
-                    q.push(cur -> right);
-                    q.push(cur -> left);
+                if(cur -> right){ //do the rightward bfs so that the right node gets inserted first so that
+                    q.push(cur -> right); //right waala pehle null pe set hoga fir right waala cur hojaega
+                    q.push(cur -> left); // fir jab left waala ayega tabtak vo right pe set hoga and we move further below
                 }
             }
         }

79f4d89437de855e0d17d95a390d15cff48c9f08
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-04-03 17:20:02 +0530

    Time: 37 ms (32.88%), Space: 11.9 MB (13.84%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -38,8 +38,8 @@ public:
                 // -------------------------------------------------------------------------
                 //Now since every level is separated by a null pointer but it contains children in a vector. What we do                   //now is traverse through that node's children and push them into the queue and repeat the same process.
                 //This whole thing continues for every level
-                for (Node* child : node->children) {
-                    q.push(child);
+                for (Node* c : node->children) {
+                    q.push(c);
                 }
             }
             ans.push_back(a);

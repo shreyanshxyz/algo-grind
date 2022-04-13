7bafd1c8b4ba9fa9ba0836f83772d46e5c2ca757
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-04-13 19:44:45 +0530

    Time: 8 ms (11.60%), Space: 9.7 MB (80.42%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -14,19 +14,22 @@ public:
     TreeNode* invertTree(TreeNode* root) {
         if(!root) return root;
         
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int sz = q.size();
-            for(int i = 0; i < sz; i++){
-                TreeNode* node = q.front();
-                q.pop();
-                swap(node->left, node->right); // we swap the left and right nodes on each level and then move to the next
-                // this simply eases any complex methods to be used and we can invert the tree in an easy manner
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-        }
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int sz = q.size();
+        //     for(int i = 0; i < sz; i++){
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         swap(node->left, node->right); // we swap the left and right nodes on each level and then move to the next
+        //         // this simply eases any complex methods to be used and we can invert the tree in an easy manner
+        //         if(node->left) q.push(node->left);
+        //         if(node->right) q.push(node->right);
+        //     }
+        // }
+        invertTree(root->left);
+        invertTree(root->right);
+        swap(root -> left, root -> right);
         return root;
     }
 };

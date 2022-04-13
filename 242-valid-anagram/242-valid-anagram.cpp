a08a8f5b5791cf94d13c42277742ba2167c2fd46
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-04-13 10:24:44 +0530

    Time: 8 ms (80.31%), Space: 7.5 MB (7.90%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s == t)
+//         BRUTE FORCE APPROACH (SORTING)
+        
+        // if (s.size() != t.size()) return false;
+
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // if(s == t)
+        //     return true;
+        // return false;
+        
+// Optimal (HASHING)
+        if (s.size() != t.size()) return false;
+        unordered_map<char, int> a, b; // we create two unordered maps
+        for (int i=0; i<s.size(); i++)
+        {
+            a[s[i]]++; // we hash the first string into the first map
+            b[t[i]]++; // we hash the second string into the second map
+        }
+        
+        if(a == b) // if both the maps match, we return true else we return false.
             return true;
         return false;
     }

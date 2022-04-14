7a521d4488077a3d73990bb35e856a59258a2d02
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-04-14 11:41:53 +0530

    Time: 43 ms (81.73%), Space: 34.7 MB (66.88%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -13,22 +13,42 @@ class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
         // brute force using bfs
+//         if(!root) return root;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty()){
+//             int sz = q.size();
+//             for(int i = 0; i < sz; i++){
+//                 TreeNode* node = q.front();
+//                 q.pop();
+                
+//                 if(node -> val == val)
+//                     return node;
+                
+//                 if(node->left) q.push(node->left);
+//                 if(node->right) q.push(node->right);
+//             }
+//         }
+//         return NULL;
+        
+// Optimal Using Recursion
         if(!root) return root;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int sz = q.size();
-            for(int i = 0; i < sz; i++){
-                TreeNode* node = q.front();
-                q.pop();
-                
-                if(node -> val == val)
-                    return node;
-                
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
+        
+        //create node to return the answer
+        TreeNode *node = new TreeNode(); 
+        
+        // if root->val != val we start to search in left and right subtrees
+        // otherwise this would be required node and we will return it
+        
+        if(val < root -> val){ // search in left 
+            node=searchBST(root -> left, val);
+        } 
+        else if(val > root -> val){ //search in right
+            node=searchBST(root->right,val);
         }
-        return NULL;
+        else{ // this is our required node
+            node=root;
+        }
+        return node;
     }
 };

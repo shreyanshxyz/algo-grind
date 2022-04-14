1633838cd5298d66e1ec9c02083dfb4227b96977
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-04-14 13:48:29 +0530

    Time: 7 ms (15.36%), Space: 10.1 MB (45.06%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -17,7 +17,7 @@ public:
         if(p == NULL || q == NULL)
             return 0;
         
-        if(p->val == q->val && p->val == q->val)
+        if(p->val == q->val )
             return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         return 0;
     }

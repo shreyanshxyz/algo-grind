ab2c7448e128b29b099f3823b3dc72e6e41356f2
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-04-17 19:02:52 +0530

    Time: 29 ms (80.75%), Space: 24 MB (98.03%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -16,12 +16,10 @@ public:
         pushAll(root);
     }
 
-    /** @return whether we have a next smallest number */
     bool hasNext() {
         return !myStack.empty();
     }
 
-    /** @return the next smallest number */
     int next() {
         TreeNode *tmpNode = myStack.top();
         myStack.pop();

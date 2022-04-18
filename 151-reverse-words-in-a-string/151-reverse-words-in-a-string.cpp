17382ac3fb73932835cb9ded46f67df0f2421de2
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-04-18 22:21:41 +0530

    Time: 7 ms (57.08%), Space: 7.1 MB (89.02%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -2,19 +2,24 @@ class Solution {
 public:
     string reverseWords(string s) {
         reverse(s.begin(), s.end());
-        int l = 0, r = 0, i = 0, n = s.size();
+// we set our left right and string size indexes
+        int l = 0, r = 0, i = 0, n = s.size(); 
+
+// now we iterate and see where how long our first word is, we set the right index and our iterator to its end
         while (i < n) {
             while (i < n && s[i] != ' ')
                 s[r++] = s[i++];
-
+// now we go through our "reversed" word and reverse it again
             if (l < r) { 
                 reverse(s.begin() + l, s.begin() + r); 
-                if (r == n) break;
-                s[r++] = ' '; 
+                if (r == n) break; // if we reach the end of the string we have completed our work
+// if not then we add a whitespace and then set our left index and the right index on the same point again
+                s[r++] = ' ';  
                 l = r;
             }
             ++i; 
         }
+// if we find our string has any underlying white spaces in the end, we then cut them off and resize our string before returning it
         if (r > 0 && s[r-1] == ' ') --r; 
         s.resize(r);
         return s;

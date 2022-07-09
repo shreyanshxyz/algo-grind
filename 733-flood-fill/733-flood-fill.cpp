20b367dda16b6c0b6cc8ddabb390c36a81787bd6
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-07-09 16:38:07 +0530

    Time: 11 ms (79.01%), Space: 14.1 MB (48.83%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     void solve(vector<vector<int>>& image, int sr, int sc, int old, int color, int row, int col){
-        if(sr < 0 || sc < 0 || sr > row || sc > col || image[sr][sc] != old) return;
-        
+        if(sr<0 || sr>=image.size() || sc<0 || sc>= image[0].size() || image[sr][sc] == col || image[sr][sc] != old)
+        {
+            return;
+        }
         image[sr][sc] = color;
-        solve(image, sr-1, sc, old, color, row, col);
-        solve(image, sr+1, sc, old, color, row, col);
-        solve(image, sr, sc-1, old, color, row, col);
-        solve(image, sr, sc+1, old, color, row, col);
+        
+        if(sr > 0 && image[sr-1][sc] == old) solve(image, sr-1, sc, old, color, row, col);
+        if(sr < row-1 && image[sr+1][sc] == old) solve(image, sr+1, sc, old, color, row, col);
+        if(sc > 0 && image[sr][sc-1] == old) solve(image, sr, sc-1, old, color, row, col);
+        if(sc < col-1 && image[sr][sc+1] == old) solve(image, sr, sc+1, old, color, row, col);
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if (image.size() == 0) return image;
         int old = image[sr][sc];
         if(old == color) return image;
-        int row = image.size()-1;
-        int col = image[0].size()-1;
-        // if (old = color) return image;
+        if(image.size() == 0) return image;
+        int row = image.size();
+        int col = image[0].size();
+        if (image.size() == 0) return image;
         solve(image, sr, sc, old, color, row, col);
         return image;
     }

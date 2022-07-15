76f66479d604e916612699c3ad47f669e31406dc
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-07-16 01:27:28 +0530

    Time: 21 ms (84.06%), Space: 23.2 MB (67.07%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -3,12 +3,14 @@ public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size(), area = 0;
         
+// (2)         We iterate through the matrix and if an element is 1, we then call a dfs function for it
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j]) {
+                if (grid[i][j] == 1) {
                     int a = 0;
                     solve(grid, i, j, a);
+// (3)                     then the max area is calculated
                     area = max(area, a);
                 }
             }
@@ -19,6 +21,7 @@ public:
     void solve(vector<vector<int>>& grid, int i, int j, int& a) {
         int m = grid.size(), n = grid[0].size();
         if (i >= 0 && i < m && j >= 0 && j < n && grid[i][j]) {
+// (1)             we increment our a for every connected cell to our first element
             a++;
             grid[i][j] = 0;            
             solve(grid, i - 1, j, a);

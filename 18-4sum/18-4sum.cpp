e23bb677a57477987febd2baefeaf79a11b72290
Shreyansh Bhadoria <51942137+shreyanshxyz@users.noreply.github.com>
2022-08-03 02:03:09 +0530

    Time: 99 ms (58.43%), Space: 13.4 MB (32.00%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
-        if(nums.empty()) return ans;
         sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>>ans;
         
-        for(int i = 0; i < nums.size(); i++){
-            long long n3 = (long long)target - (long long)nums[i];
-            for(int j = i+1; j < nums.size(); j++){
-                long long n2 = (long long)n3 - (long long)nums[j];
+        for(int i = 0; i < n; i++){
+            long long n1 = (long long)target - (long long)nums[i];
+            for(int j = i + 1; j < n; j++){
+                long long n2 = (long long)n1 - (long long)nums[j];
                 
-                int front = j+1;
-                int back = nums.size() - 1;
+                int l = j + 1;
+                int r = n -1;
                 
-                while(front < back){
-                    int x = nums[front] + nums[back];
+                while(l < r){
+                    long long ts = (long long)nums[l] + (long long)nums[r];
                     
-                    if(x < n2) front++;
-                    else if(x > n2) back--;
-                    
+                    if(ts < n2) l++;
+                    else if(ts > n2) r--;
                     else {
-                        vector<int> quadruplet(4, 0);
-                        quadruplet[0] = nums[i];
-                        quadruplet[1] = nums[j];
-                        quadruplet[2] = nums[front];
-                        quadruplet[3] = nums[back];
-                        ans.push_back(quadruplet);
+                        vector<int>v;
+                        v.push_back(nums[i]);
+                        v.push_back(nums[j]);
+                        v.push_back(nums[l]);
+                        v.push_back(nums[r]);
+                        ans.push_back(v);
                         
-                        while (front < back && nums[front] == quadruplet[2]) ++front;
-                    
-                        while (front < back && nums[back] == quadruplet[3]) --back;
+                        while(l < r && nums[l] == v[2]) l++;
+                        while(l < r && nums[r] == v[3]) r--;
                     }
                 }
-                while(j + 1 < nums.size() && nums[j + 1] == nums[j]) ++j;
+                while(j+1 < n && nums[j] == nums[j+1]) j++;
             }
-            while (i + 1 < nums.size() && nums[i + 1] == nums[i]) ++i;
+            while(i+1 < n && nums[i] == nums[i+1]) i++;
         }
         return ans;
     }
